083473813858704b3a72b41fd8c671fb9a8848ce
olivier Rukundo <rr.olivier01@gmail.com>
2025-06-01 17:44:47 +0200

    responsive ness included

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import Navbar from './Navbar'
 
 function Header() {
   return (
-    <div className='min-h-screen mb-4 bg-cover bg-center items-center w-full overfolw-hidden' style={{ backgroundImage: "url('/header_img.png')"}} id='Header'>
+    <div className='min-h-screen mb-4 bg-cover flex bg-center items-center w-full overflow-hidden' style={{ backgroundImage: "url('/header_img.png')"}} id='Header'>
     <Navbar/>
-    <div>
-        <h2>Explore homes that fit your dreams</h2>
-        <div>
-            <a href="#">Projects</a>
-            <a href="#">Contact Us</a>
+    <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>   
+        {/* alternative class option flex flex-col text-center  */}
+        <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams</h2>
+        <div className='space-x-6 mt-16'>
+            <a href="#Project" className='border border-white px-8 py-3 rounded'>Projects</a>
+            <a href="#Contact" className='border border-white bg-blue-400 px-8 py-3 rounded'>Contact Us</a>
         </div>
     </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,16 @@ function Navbar(){
                 <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonies</a>
             </ul>
             <button className='hidden md:block cursor-pointer text-white hover:bg-white rounded-full px-6 py-2 hover:text-gray-400'>Sign up</button>
+            <img src={assets.menu_icon} className='md:hidden w-7' alt="" />
+        </div>
+        {/* ---Mobile-menu---- */}
+        <div className='md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden'>
+            <ul>
+                <a href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
+                <a href="#About" className='px-4 py-2 rounded-full inline-block'>About</a>
+                <a href="#Project" className='px-4 py-2 rounded-full inline-block'>Projects</a>
+                <a href="#Testimony" className='px-4 py-2 rounded-full inline-block'>Testimonies</a>
+            </ul>
         </div>
     </div>
   )
