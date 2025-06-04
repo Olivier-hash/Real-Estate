3e9b15977f1b2b070f30b7b17895524bad9aa679
olivier Rukundo <rr.olivier01@gmail.com>
2025-06-04 19:20:34 +0200

    Arrow based direction in Project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-
+import {assets} from '../assets/assets'
 function Projects() {
   return (
-    <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 w-full overflow-hidden' id='Projects'>
-        <h1 className='text-2xl sm:text-4xl font-bold mb-2'>Projects 
+    <div className='container  mx-auto  py-4 pt-20 px-6 md:px-20 lg:px-32 w-full overflow-hidden' id='Projects'>
+        <h1 className='text-center justify-end mb-5 text-2xl sm:text-4xl font-bold mb-2'>Projects 
             <span className='underline underline-offset-4 decorated-1 under'>Completed</span>
         </h1>
-        <p>Crafting spaces, Building Legacies-Explore our portifolio</p>
+        <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting spaces, Building Legacies-Explore our portifolio</p>
+
+        {/* slider buttons */}
+
+        <div className='flex justify-end items-center mb-8'>
+          <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous project'>
+            <img src={assets.left_arrow} alt="previous" />
+          </button>
+          <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous project'>
+            <img src={assets.right_arrow} alt="Next" />
+          </button>
+        </div>
     </div>
   )
 }
