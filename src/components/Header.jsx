f69ece88349b31e4617df5392d693feb31df3041
olivier Rukundo <rr.olivier01@gmail.com>
2025-06-19 16:47:30 +0200

    Font and animation from header added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
     
     className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>   
         {/* alternative class option flex flex-col text-center  */}
-        <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'> made <br /> by <br />Olivier Rukundo</h2>
+        <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'> Made by Olivier</h2>
         <div className='space-x-6 mt-16'>
             <a href="#Project" className='border border-white px-8 py-3 rounded'>Projects</a>
             <a href="#Contact" className='border border-white bg-blue-400 px-8 py-3 rounded'>Contact Us</a>
